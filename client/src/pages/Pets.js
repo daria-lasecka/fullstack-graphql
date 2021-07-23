bb02f821c6cebb463cfe28665edde5c76d10d60a
Daria Lasecka <daria.lasecka@htdevelopers.com>
2021-07-23 16:15:18 +0200

    Finished GraphQL course

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,15 +5,79 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    type
+    img
+    vaccinated @client
+    owner {
+      id
+      age @client
+    }
+  }
+`
+
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${ PETS_FIELDS }
+`
+
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${ PETS_FIELDS }
+`
+
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const { data, loading, error } = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, {data: { addPet }}) {
+      const data = cache.readQuery({ query: ALL_PETS });
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...data.pets] }
+      })
+    }
+  })
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: { newPet: input },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: Math.floor(Math.random() * 10000) + '',
+          name: input.name,
+          type: input.type,
+          img: 'https://via.placeholder.com/300'
+        }
+      }
+    })
   }
-  
+
+  if(loading) {
+    return <Loader />
+  }
+
+  if(error || newPet.error) {
+    return <p>error!</p>
+  }
+
+  console.log(data.pets[0])
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +96,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
